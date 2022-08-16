436c1de16ca344e49a15091774d02786266177b8
Juan Valencia <67487522+Juan-Valencia98@users.noreply.github.com>
2022-08-16 10:09:09 -0400

    Update 04-listas.dart
    Agregando mas datos

--- dart-basico/04-listas.dart
+++ dart-basico/04-listas.dart
@@ -5,6 +5,10 @@ void main() {
   numeros.add(11);
   print( numeros );
   
+  print(numeros[0]);
+  for (var item in numeros) {
+    print(item);
+  }
   final masNumeros = List.generate(100, (int index) => index );
  
   print(masNumeros);
